5da05709a9849477dcefc595383c4231a971cbfb
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-03-21 17:19:05 -0300

    resolução com circle

---
+++ Processamento de Imagens/Atividades/Aula 03/desafio_02_circle.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+
+BLUE = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+COLORS=[BLUE,GREEN]
+points = []
+
+thickness = 10
+color = BLUE
+
+def draw(event, x, y, flags, param):
+    global points
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        points.append(x), points.append(y)
+
+video = cv2.VideoCapture("Processamento de Imagens/videos/IFMA Campus Caxias.mp4")
+
+frameWidth = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+frameHeight = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+fps = video.get(cv2.CAP_PROP_FPS)
+
+cv2.namedWindow('Video')
+
+if not video.isOpened():
+    print('Erro ao acessar!')
+else:
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    newVideo = cv2.VideoWriter("Processamento de Imagens/videos/new_video.mp4", fourcc,
+                               int(fps), (int(frameWidth), int(frameHeight)))
+
+    while video.isOpened():
+        ret, frame = video.read()
+        if ret is True:
+            cv2.setMouseCallback('Video', draw)
+
+            key = cv2.waitKey(1)
+
+            if key & 0xFF == ord('c'):
+                if color == BLUE:
+                    color = GREEN
+                else:
+                    color = BLUE
+
+            elif key & 0xFF == ord(' '):
+                points.clear()
+
+            if points:
+                for i in range(0, len(points), 2):
+                     cv2.circle(frame, (points[i], points[i+1]), 10, color, -1)
+    
+            newVideo.write(frame)
+
+            cv2.imshow('Video', frame)
+
+            if key & 0xFF == ord('q'):
+                break
+
+        else: break
+
+video.release()
+newVideo.release()
+cv2.destroyAllWindows()
