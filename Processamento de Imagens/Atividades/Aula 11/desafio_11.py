d3cff22d6465b62469d9f0ce067e2ba1265e6f02
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-05-07 13:25:52 -0300

    Atividade 11

---
+++ Processamento de Imagens/Atividades/Aula 11/desafio_11.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('Processamento de Imagens/images/atividade_aula11.png', cv2.IMREAD_GRAYSCALE)
+img = cv2.resize(img, (200, 200), interpolation = cv2.INTER_CUBIC)
+
+kernel = np.ones((9, 9), dtype = np.uint8)
+kernelDilation = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
+
+# erosão da primeira modificação
+erosion = cv2.erode(img, kernel, iterations = 3)
+# dilatação necessária para a terceira imagem
+dilation = cv2.dilate(img, kernel, iterations = 5)
+# erosão aplicada a imagem dilatada anteriormente com o kernel de elipse
+erosion_kd = cv2.erode(dilation, kernelDilation, iterations = 5)
+# dilatação eliptica aplicada a erosão feita na linha anterior
+dilation_rounded = cv2.dilate(erosion_kd, kernelDilation, iterations = 3)
+
+plt.subplot(221), plt.imshow(img), plt.title("Original")
+plt.subplot(222), plt.imshow(erosion), plt.title("Erosion")
+plt.subplot(223), plt.imshow(dilation_rounded), plt.title("Dilation")
+
+plt.tight_layout()
+plt.show()
