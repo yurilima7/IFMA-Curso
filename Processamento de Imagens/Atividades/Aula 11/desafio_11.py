d36224d375f156075bd3301e8c783f8c976fca98
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-05-10 07:36:37 -0300

    finalização

--- Processamento de Imagens/Atividades/Aula 11/desafio_11.py
+++ Processamento de Imagens/Atividades/Aula 11/desafio_11.py
@@ -7,6 +7,9 @@ img = cv2.resize(img, (200, 200), interpolation = cv2.INTER_CUBIC)
 
 kernel = np.ones((9, 9), dtype = np.uint8)
 kernelDilation = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
+kernelElipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
+kernelClip = np.ones((16, 9), dtype = np.uint8)
+kernelClipEnd = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (25, 25))
 
 # erosão da primeira modificação
 erosion = cv2.erode(img, kernel, iterations = 3)
@@ -17,9 +20,17 @@ erosion_kd = cv2.erode(dilation, kernelDilation, iterations = 5)
 # dilatação eliptica aplicada a erosão feita na linha anterior
 dilation_rounded = cv2.dilate(erosion_kd, kernelDilation, iterations = 3)
 
+# primeira erosão de separação
+erosion_clip = cv2.erode(img, kernelClip, iterations = 2)
+# segunda erosão de separação
+erosion_clip_end = cv2.erode(erosion_clip, kernelClipEnd, iterations = 1)
+# dilatação de corte final
+dilation_rounded_clip = cv2.dilate(erosion_clip_end, kernelElipse, iterations = 1)
+
 plt.subplot(221), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)), plt.title("Original")
 plt.subplot(222), plt.imshow(cv2.cvtColor(erosion, cv2.COLOR_BGR2RGB)), plt.title("Erosion")
 plt.subplot(223), plt.imshow(cv2.cvtColor(dilation_rounded, cv2.COLOR_BGR2RGB)), plt.title("Dilation")
+plt.subplot(224), plt.imshow(cv2.cvtColor(dilation_rounded_clip, cv2.COLOR_BGR2RGB)), plt.title("Dilation Clip")
 
 plt.tight_layout()
 plt.show()
